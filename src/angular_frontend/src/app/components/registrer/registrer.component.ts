c2e6787f613e51b6c9cae23dfd7604af787f36d0
Sindre Johan Imenes Sivertsen <sindrejohan@live.no>
2018-09-06 23:39:39 +0200

    started on registrer logic

--- src/angular_frontend/src/app/components/registrer/registrer.component.ts
+++ src/angular_frontend/src/app/components/registrer/registrer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import {  ApiService } from 'services/api.service';
+import { Player } from 'app/model';
 
 @Component({
   selector: 'app-registrer',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrer.component.css']
 })
 export class RegistrerComponent implements OnInit {
+  registrerForm: FormGroup;
+  player: Player;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    
+   }
 
   ngOnInit() {
   }
