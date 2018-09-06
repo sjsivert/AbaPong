e679fad9dde8eedb7c7908dc88db278376558bfa
Sindre Johan Imenes Sivertsen <sindrejohan@live.no>
2018-09-06 13:49:17 +0200

    first commit

---
+++ src/abapong/leaderboard/serializers.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+from abapong.leaderboard.models import Player
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = ('id', 'name', 'elo', 'created', 'rfidTag')
+
+'''class PlayerSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(required=True, allow_blank=False, max_length=30)
+    elo = serializers.IntegerField()
+
+    def create(self, validate_data):
+        return Player.objects.create(**validate_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+'''
